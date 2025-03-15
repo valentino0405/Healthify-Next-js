a4400d7dc862ece50cebb9f72a85d4993ae07e11
valentino0405 <gomesvalentino742@gmail.com>
2025-03-15 20:49:36 +0530

    Till 2hours 54 mins

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -3,12 +3,14 @@ import { db } from '@/utils/db';
 import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
 import React, { useEffect ,useState} from 'react';
+import QuestionsSection from './_components/QuestionsSection';
+import RecordAnswerSection from './_components/RecordAnswerSection';
 
 function StartInterview({params}) {
 
     const[interviewData,setInterviewData]=useState();
     const[mockInterviewQuestion,setMockInterviewQuestion]=useState();
-
+    const [activeQuestionIndex,setActiveQuestionIndex]=useState(0);
     useEffect(()=>{
             GetInterviewDetails();
     },[]);
@@ -18,7 +20,7 @@ function StartInterview({params}) {
       const GetInterviewDetails = async () => {
         const result = await db.select().from(MockInterview)
           .where(eq(MockInterview.mockId, params.interviewId))
-    
+   
         const jsonMockResp=JSON.parse(result[0].jsonMockResp)
         console.log(jsonMockResp);
         setMockInterviewQuestion(jsonMockResp);
@@ -27,7 +29,19 @@ function StartInterview({params}) {
 
   return (
     <div>
-      Start Interview
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+              {/*Questions*/}
+              <QuestionsSection 
+              mockInterviewQuestion={mockInterviewQuestion}
+              activeQuestionIndex={activeQuestionIndex}/>
+              {/* Video/audio Recording*/}
+              <RecordAnswerSection
+                mockInterviewQuestion={mockInterviewQuestion}
+                activeQuestionIndex={activeQuestionIndex}
+                interviewData={interviewData}
+              />
+        </div>
+    
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider,} from '@clerk/nextjs'
+import { Toaster } from "@/components/ui/sonner";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><Toaster/>
         {children}
       </body>
     </html>
