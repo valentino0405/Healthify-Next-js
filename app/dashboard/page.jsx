3e74f2e809308d8fdc5be52ae81e8dd252941b77
valentino0405 <gomesvalentino742@gmail.com>
2025-03-22 21:30:47 +0530

    added a page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,7 +8,7 @@ function Dashboard() {
     <div className='p-10'>
       <h2 className='font-bold text-2xl'>Dashboard</h2>
       <h2 className='text-gray-500'>Create and Tell Your Health Problem to AI</h2>
-    
+      
       <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
           <AddNewInterview/>
       </div>
